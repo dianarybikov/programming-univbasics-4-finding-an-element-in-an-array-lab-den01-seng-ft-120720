88d1783663864dc909f90aa7ac68e100d5120175
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-20 21:07:38 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,10 +2,8 @@ def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length do
     if value_to_find == array[count]
-      returns 
-      
-     
-     
+      returns count
+    end
   count += 1    
 end
 end
